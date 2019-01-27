d876f275ed7a35526faa0a6a46acfb75031a2bbd
Maximilian Pachl <faryon93@users.noreply.github.com>
2019-01-27 12:31:53 +0000

    feat(logline): properly display escaped quotes

--- fields.js
+++ fields.js
@@ -1,24 +1,44 @@
 // ==UserScript==
 // @name         LogDNA Formater
 // @namespace    https://github.com/faryon93/
-// @version      0.1
+// @version      0.2
 // @description  Reformat LogDNA fields according to Xorbits Logformat
 // @author       Maximilian Pachl
 // @match        https://app.logdna.com/*/logs/view*
 // @grant        none
 // ==/UserScript==
 
+// ------------------------------------------------------------------------------------------------
+function logline(elem)
+{
+    if (!elem || !elem.getElementsByClassName)
+        return;
+
+    for (let q of elem.getElementsByClassName("base-text"))
+        q.innerHTML = q.innerHTML.replace(/\\\"/g, "\"");
+}
+
+// ------------------------------------------------------------------------------------------------
+function trace(elem)
+{
+    elem.innerHTML = elem.innerHTML.replace("\\n", "<br>");
+}
+
+// ------------------------------------------------------------------------------------------------
 (function() {
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
-            if (!mutation.target.classList.contains("log-line-context"))
-                return;
-
-            for (let q of mutation.target.getElementsByClassName("line-field-dt"))
+            if (mutation.target.classList.contains("log-content"))
+                mutation.addedNodes.forEach(logline);
+            else if (mutation.target.classList.contains("log-line-context"))
             {
-                let content = q.parentNode.nextElementSibling;
-                if (q.innerHTML == "trace")
-                    content.innerHTML = content.innerHTML.replace("\\n", "<br>");
+                let fields = mutation.target.getElementsByClassName("line-field-dt");
+                for (let field of fields)
+                {
+                    let content = field.parentNode.nextElementSibling;
+                    if (field.innerHTML == "trace")
+                        trace(content);
+                }
             }
         });
     });
